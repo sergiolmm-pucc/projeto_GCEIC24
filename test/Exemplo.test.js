1c44c3ebb5a2a87f3d39409082d28d221fd42b65
Sergio Marques <sergio.marques@puc-campinas.edu.br>
2024-05-17 15:39:51 -0300

    erro jest test file fixed by slmm

--- test/Exemplo.test.js
+++ test/Exemplo.test.js
@@ -3,7 +3,7 @@ const {calcularArea} = require('../public/javascripts/exemplo');
 describe('Teste exemplo 1', () => {
 
     test('Funcao calcularArea', ()=>{
-        expect(calcularArea(2,3).toBe(6);
+        expect(calcularArea(2,3)).toBe(6);
     })
 
 })
