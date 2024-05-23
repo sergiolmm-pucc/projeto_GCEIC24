f08b5b26b1465d83ae5d0c3eb11674c5af659162
TOMÁS FILIZOLA XAVIER <tomas.fx@puccampinas.edu.br>
2024-05-23 09:28:23 -0300

    teste

--- test/calculadora.test.js
+++
@@ -1,9 +0,0 @@
-const {calcularRendimento} = require('../public/javascripts/calculadora');
-
-describe('Teste execução calculadora', () => {
-
-    test('Funcao calcularRendimento', ()=>{
-        expect(calcularRendimento(500,5,12)).toBe(897.93);
-    })
-
-})
