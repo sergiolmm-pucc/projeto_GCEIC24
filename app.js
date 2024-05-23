33d8c9418e49af9dbb587bde4eef8cb2dd1e0215
Guilherme Jorge <guilherme.jorge010@gmail.com>
2024-05-23 20:39:01 -0300

    fix: arrumei o commit que sobrescreveu as mudanças feitas hoje

--- app.js
+++ app.js
@@ -1,36 +1,60 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var exampleRouter = require('./routes/example');
-var ifspRouter = require('./routes/infsRoute');
+
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var exampleRouter = require("./routes/example");
+
+var calculadoraRouter = require("./routes/calculadora");
+var caloricExpenditureRouter = require("./routes/caloricExpendieture");
+
+var devOps3MarkUpRouter = require("./routes/dev-ops-3-mark-up");
+
+var geomcalcRouter = require("./routes/geomcalcRouter");
+
+var infsRouter = require("./routes/infsRoute");
+var irrfRouter = require("./routes/irrf.routes");
+
+var SDIRouter = require("./routes/SDI.js");
+
+var temperatureRouter = require("./routes/temperature");
+var tmbRouter = require("./routes/tmb.routes");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Definir rotas para arquivos HTML específicos
-app.get('/infs', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/html/infs.html'));
-});
+app.use(express.static(path.join(__dirname, "public")));
 
 // Definir suas rotas
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/example', exampleRouter);
-app.use('/infs', ifspRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/example", exampleRouter);
+
+app.use("/calculadora", calculadoraRouter);
+app.use("/caloricExpenditure", caloricExpenditureRouter);
+
+app.use("/dev-ops-3-mark-up", devOps3MarkUpRouter);
+
+app.use("/geomcalc", geomcalcRouter);
+
+app.use("/irrf", irrfRouter);
+app.use("/infs", infsRouter);
+
+app.use("/SDI", SDIRouter);
+
+app.use("/temperature", temperatureRouter);
+app.use("/tmb", tmbRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -38,18 +62,14 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function(err, req, res) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
-});
-
-app.listen(4000, () => {
-  console.log("app listening on port 4000");
+  res.render("error");
 });
 
 module.exports = app;
