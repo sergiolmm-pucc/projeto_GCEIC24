c1d623985aaecbf9b2109e99388435f7f8da3a06
Luan Tarran <luan.mtarran@gmail.com>
2024-05-23 23:29:14 -0300

    rollback app.js

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+// Definir suas rotas
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/example", exampleRouter);
@@ -56,9 +57,11 @@ app.use("/infs", infsRouter);
 app.use("/planetweight",planetweightrouter)
 
 app.use("/roi", roiRouter);
+
 app.use("/SDI", SDIRouter);
-app.use("/tmb", tmbRouter);
+
 app.use("/temperature", temperatureRouter);
+app.use("/tmb", tmbRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
