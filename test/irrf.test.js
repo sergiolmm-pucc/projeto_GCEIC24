e0822b0876752f2fa8c53f3b19d138048b91d896
Guilherme Jorge <guilherme.jorge010@gmail.com>
2024-05-25 23:37:27 -0300

    fix: ajustei a colocação da função toBe no teste unitário do irrf

--- test/irrf.test.js
+++ test/irrf.test.js
@@ -2,7 +2,7 @@ const { calculoIRRF } = require("../public/javascripts/irrf");
 
 describe("Teste exemplo IRRF", () => {
   test("Função calcularIRRF", () => {
-    expect(calculoIRRF(3000).toBe([47.19, 258.82]));
-    expect(calculoIRRF(5000).toBe([356.54, 518.82]));
+    expect(calculoIRRF(3000)).toBe([47.19, 258.82]);
+    expect(calculoIRRF(5000)).toBe([356.54, 518.82]);
   });
 });
