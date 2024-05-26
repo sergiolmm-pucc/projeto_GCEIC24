0c153bdc208a8471af7fd18318da839a1bebc84c
Guilherme Jorge <guilherme.jorge010@gmail.com>
2024-05-25 23:32:08 -0300

    feat: ajustei o nome da função que estava errado no teste unitário

--- test/irrf.test.js
+++ test/irrf.test.js
@@ -1,8 +1,8 @@
-const { calcularIRRF } = require("../public/javascripts/irrf");
+const { calculoIRRF } = require("../public/javascripts/irrf");
 
 describe("Teste exemplo IRRF", () => {
   test("Função calcularIRRF", () => {
-    expect(calcularIRRF(3000).toBe([47.19, 258.82]));
-    expect(calcularIRRF(5000).toBe([356.54, 518.82]));
+    expect(calculoIRRF(3000).toBe([47.19, 258.82]));
+    expect(calculoIRRF(5000).toBe([356.54, 518.82]));
   });
 });
