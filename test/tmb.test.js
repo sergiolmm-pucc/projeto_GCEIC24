e86a9131f2318d9fe84f79c65f924e82683a4129
nicholastomic <nicholasrrtomic@gmail.com>
2024-05-30 16:42:02 -0300

    Adição do código para tmbfuncional

--- test/tmb.test.js
+++ test/tmb.test.js
@@ -1,34 +1,21 @@
-const { tmb } = require("../public/javascripts/tmb");
+const { calcularTMBHomem } = require("../public/javascripts/tmb");
+
+const { calcularFatorAtividade } = require("../public/javascripts/tmb");
 
 
 describe("Teste calculo de TMB", () => {
-    let calculoTMB;
-
-    beforeEach(() => {
-        calculoTMB = new tmb();
-    });
-
     test('calculo de TMB para homem de 20 anos, 100 kgs e 180 cm', () => {
 
-        const calularTMBHomem = calculoTMB.calcularTMBHomem(100, 180, 20);
+        const calularTMBHomem = calcularTMBHomem(100, 180, 20);
 
-        const fatorAtividade = calculoTMB.calcularFatorAtividade("sedentario")
-
-        const taxa = calularTMBHomem*fatorAtividade
-
-        expect(taxa).toEqual(2640)
-    });
-
-    test('calculo de TMB para mulher de 20 anos, 100 kgs e 180 cm', () => {
-
-        const calularTMBHomem = calculoTMB.calcularTMBMulher(100, 180, 20);
-
-        const fatorAtividade = calculoTMB.calcularFatorAtividade("sedentario")
+        const fatorAtividade = calcularFatorAtividade("sedentario")
 
         const taxa = calularTMBHomem*fatorAtividade
 
-        expect(taxa).toEqual(2214)
+        expect(taxa).toBe(2640)
     });
+
+
 
 
 });
