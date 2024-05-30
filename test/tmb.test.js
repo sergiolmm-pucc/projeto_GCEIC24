18d691173be88a688d96978de00ce44fd318b196
nicholastomic <nicholasrrtomic@gmail.com>
2024-05-30 17:59:15 -0300

    Mudanças no script tmb, correção de erro no teste unitário (não reconhecimento da variavel "document")

--- test/tmb.test.js
+++ test/tmb.test.js
@@ -1,21 +1,15 @@
 const { calcularTMBHomem } = require("../public/javascripts/tmb");
 
-const { calcularFatorAtividade } = require("../public/javascripts/tmb");
-
+const { calcularFatorAtividade } = require("../public/javascripts/tmb")
 
 describe("Teste calculo de TMB", () => {
     test('calculo de TMB para homem de 20 anos, 100 kgs e 180 cm', () => {
 
-        const calularTMBHomem = calcularTMBHomem(100, 180, 20);
+        const tmbhomem = calcularTMBHomem(100, 180, 20);
+        const fator = calcularFatorAtividade("sedentario");
 
-        const fatorAtividade = calcularFatorAtividade("sedentario")
-
-        const taxa = calularTMBHomem*fatorAtividade
-
+        const taxa = fator*tmbhomem
         expect(taxa).toBe(2640)
     });
-
-
-
 
 });
