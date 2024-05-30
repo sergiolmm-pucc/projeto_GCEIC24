9eb89abfc1bc815371f908271c2d7fdddd315df8
Eric Abib <ehheric@gmail.com>
2024-05-30 16:13:59 -0300

    Grupo SDI - Test e funcional finalizado e funcionando

--- test/SDI.test.js
+++ test/SDI.test.js
@@ -3,7 +3,7 @@ const { InvestimentoSeguro } = require('../public/javascripts/SDI')
 describe('Teste SDI - Simulador de Investimento', () => {
 
     test('Funcao Simulador de Investimento Seguro', ()=>{
-        expect(InvestimentoSeguro(10, 500, 6)).toBe(3.857,81);
+        expect(InvestimentoSeguro(10, 500, 6)).toBe(3857.81);
     })
 
 })
