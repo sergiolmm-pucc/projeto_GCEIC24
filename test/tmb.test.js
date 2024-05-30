9224513936dc336fc3b982adab796cbd5a8f81d6
nicholastomic <nicholasrrtomic@gmail.com>
2024-05-30 18:17:22 -0300

    Adição de teste de mulher de 17 anos, 100kgs e 165 cm

--- test/tmb.test.js
+++ test/tmb.test.js
@@ -2,7 +2,9 @@ const { calcularTMBHomem } = require("../public/javascripts/tmb");
 
 const { calcularFatorAtividade } = require("../public/javascripts/tmb")
 
-describe("Teste calculo de TMB", () => {
+const { calcularTMBMulher } = require("../public/javascripts/tmb")
+
+describe("Testes de calculo de TMB", () => {
     test('calculo de TMB para homem de 20 anos, 100 kgs e 180 cm', () => {
 
         const tmbhomem = calcularTMBHomem(100, 180, 20);
@@ -12,4 +14,12 @@ describe("Teste calculo de TMB", () => {
         expect(taxa).toBe(2640)
     });
 
+    test('calculo de TMB para mulher de 17 anos, 100 kgs e 165 cm', () => {
+
+        const tmbhomem = calcularTMBMulher(100, 165, 17);
+        const fator = calcularFatorAtividade("sedentario");
+
+        const taxa = fator*tmbhomem
+        expect(taxa).toBe(2198.52)
+    });
 });
