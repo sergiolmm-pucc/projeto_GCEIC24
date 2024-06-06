60b632e8c045c54015f4c866e0da3fc2d1e61bdb
Francisco Cardoso de Oliveira Filho <francisco.cof@puccampinas.edu.br>
2024-06-06 08:47:01 -0300

    update

--- public/javascripts/contForca.js
+++ public/javascripts/contForca.js
@@ -1,20 +1,21 @@
-// contForca.js
 function calcularNovoPeso(currentWeight, exerciseFactor, difficultyFactor) {
     let newWeight = currentWeight * exerciseFactor * difficultyFactor;
     return Math.round(newWeight);
 }
 
-if (typeof document !== 'undefined') {
-    document.addEventListener('DOMContentLoaded', function () {
-        document.getElementById('weightForm').addEventListener('submit', function(event) {
-            event.preventDefault();
-            const currentWeight = parseFloat(document.getElementById('currentWeight').value);
-            const exerciseFactor = parseFloat(document.getElementById('exercise').value);
-            const difficultyFactor = parseFloat(document.getElementById('difficulty').value);
-            const newWeight = calcularNovoPeso(currentWeight, exerciseFactor, difficultyFactor);
-            document.getElementById('result').innerText = `Peso sugerido para a próxima sessão: ${newWeight} kg`;
-        });
+function contabilidadeForca() {
+    document.getElementById('weightForm').addEventListener('submit', function(event) {
+        event.preventDefault();
+        const currentWeight = parseFloat(document.getElementById('currentWeight').value);
+        const exerciseFactor = parseFloat(document.getElementById('exercise').value);
+        const difficultyFactor = parseFloat(document.getElementById('difficulty').value);
+        const newWeight = calcularNovoPeso(currentWeight, exerciseFactor, difficultyFactor);
+        document.getElementById('result').innerText = `Peso sugerido para a próxima sessão: ${newWeight} kg`;
     });
+}
+
+if (typeof document !== 'undefined') {
+    document.addEventListener('DOMContentLoaded', contabilidadeForca);
 }
 
 module.exports = { calcularNovoPeso, contabilidadeForca };
