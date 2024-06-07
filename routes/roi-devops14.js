93e05cc5b225ea9aa87b100806b74c5aa6251913
manoel <manoel.albino@proton.me>
2024-06-07 17:55:46 -0400

    added missing files

--- routes/roi-devops14.js
+++ routes/roi-devops14.js
@@ -1,34 +1,35 @@
 var express = require("express");
 var router = express.Router();
-var zod = require("zod");
 const fs = require("fs");
+const { roiSchema, calculateRoi } = require("../services/roi-devops14-service");
+//var zod = require("zod");
 
 // Validação dos valores de entrada
 // TODO: Verificar se funciona com o Gitch
 // TODO: Adicionar o zod no package.json
 
-const roiSchema = zod.object({
-  ganhoInvestimento: zod.union([
-    zod
-      .string()
-      .min(1)
-      .transform((value) => Number(value)),
-    zod.number().min(1),
-  ]),
-  custoInvestimento: zod.union([
-    zod
-      .string()
-      .min(1)
-      .transform((value) => Number(value)),
-    zod.number().min(1),
-  ]),
-});
+// const roiSchema = zod.object({
+//   ganhoInvestimento: zod.union([
+//     zod
+//       .string()
+//       .min(1)
+//       .transform((value) => Number(value)),
+//     zod.number().min(1),
+//   ]),
+//   custoInvestimento: zod.union([
+//     zod
+//       .string()
+//       .min(1)
+//       .transform((value) => Number(value)),
+//     zod.number().min(1),
+//   ]),
+// });
 
 // Rota para verificar a integridade
 router.get("/", (_req, res) => {
   fs.readFile("./public/html/roi-devops14.html", function(err, html) {
     if (err) {
-      throw err;
+      res.status(500).send("Erro ao carregar a página HTML");
     } else {
       res.writeHeader(200, { "Content-Type": "text/html" });
       res.write(html);
@@ -39,16 +40,20 @@ router.get("/", (_req, res) => {
 
 // Rota para calcular o ROI
 router.post("/calcular", (req, res) => {
-  const { ganhoInvestimento, custoInvestimento } = roiSchema.parse(req.body);
+  try {
+    const result = roiSchema.safeParse(req.body);
+    if (!result.success) {
+      return res.status(400).send("Parâmetros inválidos");
+    }
 
-  //const { ganhoInvestimento, custoInvestimento } = req.body;
+    const { ganhoInvestimento, custoInvestimento } = result.data;
 
-  const roi =
-    ((Number(ganhoInvestimento) - Number(custoInvestimento)) /
-      Number(custoInvestimento)) *
-    100;
+    const roi = calculateRoi(ganhoInvestimento, custoInvestimento);
 
-  return res.status(200).send({ roi });
+    return res.status(200).send({ roi });
+  } catch (res) {
+    return res.status(500).send("Erro no servidor");
+  }
 });
 
 module.exports = router;
